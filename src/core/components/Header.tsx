7005546d44925c95f39004c782988094a296a67a
Stiven <stivenpilca@gmail.com>
2025-02-27 18:05:49 -0500

    feat: change header functionality Change header conditional render buttons and style

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -4,37 +4,50 @@ import logo from '../../assets/car.png';
 import SearchBar from "../../modules/cars/components/SearchBar";
 import SearchResultList from "../../modules/cars/components/SearchResultList";
 import { useState } from "react";
-import { useLocation } from 'react-router-dom'
-import { motion } from "motion/react";
 import { useAuth } from "../../context/AuthContext";
+import { TbGitCompare, TbLogin2, TbLogout } from "react-icons/tb";
+import { FaCartFlatbed } from "react-icons/fa6";
 
-export default function Header({ isVisible }) {
-
-    const { user } = useAuth();
-    const location = useLocation();
+export default function Header() {
+    const { user, logout } = useAuth();
     const [searchResults, setSearchResults] = useState([]);
 
     return (
-        <div className={`${styles.headerContainer} ${isVisible ? styles.blur : ""}`}>
+        <div className={styles.headerContainer}>
             <div className={styles.headerLeft}>
                 <Link to={'/'}>
                     <div className={styles.logoContainer}>
                         <img src={logo} className={styles.logoImg}></img>
                     </div>
                 </Link>
-                {/* <div className={styles.welcome}>
-                    Bienvenido
-                </div> */}
                 <div className={styles.searchBarContainer}>
-                    <SearchBar setResults={setSearchResults}></SearchBar>
-                    <SearchResultList results={searchResults}></SearchResultList>
+                    <SearchBar
+                        setResults={setSearchResults}
+                    ></SearchBar>
+                    <SearchResultList
+                        results={searchResults}
+                    ></SearchResultList>
                 </div>
             </div>
             <div className={styles.headerButtonContainer}>
                 <nav className={styles.navbar}>
-                    <Link to={'/car/compare'} className={styles.headerOption}>Comparar</Link>
-                    {user && <Link to={'/user/cart'} className={`${styles.headerOption, location.pathname === '/car/compare' ? 'active' : ''}`}>Carrito</Link>}
-                    {user? <a>Mi cuenta</a> : <Link to={'/login'} className={styles.headerOption}>Iniciar Sesión</Link>}
+                    {user &&
+                        <div className={styles.headerOption}>
+                            <TbGitCompare />
+                            <Link to={'/car/compare'}>Comparar</Link>
+                        </div>
+                        // <div className={styles.headerOption}>
+                        //     <FaCartFlatbed />
+                        //     <Link to={'/user/cart'} className={styles.headerOption}>Carrito</Link>
+                        // </div>
+                    }
+                    <div className={styles.headerOption}>
+                        {user ?
+                            <Link to={'/'} onClick={() => logout()}><TbLogout />
+                                Cerrar Sesión</Link> :
+                            <><TbLogin2 />
+                                <Link to={'/login'} className={styles.headerOption}>Iniciar Sesión</Link></>}
+                    </div>
                 </nav>
             </div>
         </div>
